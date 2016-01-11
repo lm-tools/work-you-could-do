73e3ed9577ff76fc9539f54ffd8d6cd3c0f2b745
Camille Baldock <camille@camillebaldock.co.uk>
2016-01-11 14:42:45 +0000

    Style SOC code selection

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'start#index'
 
+  get 'select_soc_codes' => 'start#select_soc_codes'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
