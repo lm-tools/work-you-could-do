d7229198b05410244a82e13fdc4cb90047b806d7
camilleldn <camille@camillebaldock.co.uk>
2016-01-12 21:50:34 +0000

    n.e.c. formatting

--- app/models/soc_occupation.rb
+++ app/models/soc_occupation.rb
@@ -5,8 +5,8 @@ class SocOccupation < ActiveRecord::Base
     soc_url = "http://api.lmiforall.org.uk/api/v1/soc/code/#{soc_code}"
     lmi_occupation = HTTParty.get(soc_url)
     self.create(
-      title: lmi_occupation["title"],
-      description: lmi_occupation["description"],
+      title: friendly_soc_title(lmi_occupation["title"]),
+      description: friendly_soc_description(lmi_occupation["description"]),
       tasks: lmi_occupation["tasks"],
       qualifications: lmi_occupation["qualifications"],
       additional_titles: clean_add_titles(lmi_occupation["add_titles"]),
@@ -17,17 +17,27 @@ class SocOccupation < ActiveRecord::Base
 
   private
 
+  def self.friendly_soc_title(title)
+    heading_re = /n\.e\.c\.\s*$/
+    title.gsub(heading_re, '').strip
+  end
+
+  def self.friendly_soc_description(description)
+    description_re = /not elsewhere classified in .*\.$/
+    description.gsub(description_re, '')
+  end
+
   def self.clean_add_titles(titles)
     formattable_titles = titles.select do |title|
       title.length >= 4 && !title.include?("(")
-    end
+    end[0..19]
     formattable_titles.map do |formattable_title|
       if formattable_title.include?(",")
         a, b = formattable_title.split(",", 1)
         formattable_title = "#{b} #{a}"
       end
-      formattable_title.strip.capitalize
-    end.join("; ")
+      formattable_title.strip.titleize
+    end.join(", ")
   end
 
 end
