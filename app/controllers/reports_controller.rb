79bc40cc80578257c889ccc8bba3b3b27c1dca67
camilleldn <camille@camillebaldock.co.uk>
2016-01-13 18:00:50 +0000

    Report mailer

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -29,11 +29,12 @@ class ReportsController < ApplicationController
 
   def email
     @report = Report.where(guid: params[:id]).first
-    if params[:email].empty?
+    @address = params[:email]
+    if @address.empty?
       @error = 'Please provide an email address.'
       render :show
     else
-      ReportMailer.email(params[:email], @report)
+      send_report_email(params[:email])
       @notice = "Sent to #{params[:email]}"
       render :show
     end
@@ -45,5 +46,30 @@ class ReportsController < ApplicationController
     occupation_params.keys.map do |occupation|
       /occupation_(\d+)/.match(occupation)[1].to_i
     end
+  end
+
+  def send_report_email(address)
+    mail = Mail.new do
+      from ENV["EMAIL_SENDER"]
+      subject 'Work you could do'
+    end
+    report_view_folder = "#{Rails.root}/app/views/reports/"
+    text = File.open("#{report_view_folder}report_email.txt.erb").read
+    text_template = ERB.new(text)
+    text_content = text_template.result(binding)
+    text_part = Mail::Part.new do
+      body text_content
+    end
+    html = File.open("#{report_view_folder}report_email.html.erb").read
+    html_template = ERB.new(html)
+    html_content = html_template.result(binding)
+    html_part = Mail::Part.new do
+      content_type 'text/html; charset=UTF-8'
+      body html_content
+    end
+    mail["to"] = address
+    mail.text_part = text_part
+    mail.html_part = html_part
+    mail.deliver!
   end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+require 'mail'
+
+Mail.defaults do
+  delivery_method :smtp, {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
