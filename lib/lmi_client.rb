a38d4e2d4fe5264169c3a7424c32658fdab886bf
camilleldn <camille@camillebaldock.co.uk>
2016-01-14 12:14:57 +0000

    Use HTTParty base_uri syntax

--- lib/lmi_client.rb
+++ lib/lmi_client.rb
@@ -1,31 +1,26 @@
 class LmiClient
-  BASE_URL = 'http://api.lmiforall.org.uk/api/v1/'
+  include HTTParty
+  base_uri 'api.lmiforall.org.uk/api/v1'
 
   def occupation_search(keyword)
-    HTTParty.get(BASE_URL + 'soc/search', query: { q: keyword })
+    self.class.get('/soc/search', query: { q: keyword })
   end
 
   def hours_lookup(soc_code)
-    request(
-      "ashe/estimateHours?soc=#{soc_code}&coarse=true"
+    self.class.get(
+      "/ashe/estimateHours?soc=#{soc_code}&coarse=true"
     )['series'][0]['hours']
   end
 
   def pay_lookup(soc_code)
-    request(
-      "ashe/estimatePay?soc=#{soc_code}&coarse=true"
+    self.class.get(
+      "/ashe/estimatePay?soc=#{soc_code}&coarse=true"
     )['series'][0]['estpay']
   end
 
   def soc_code_lookup(soc_code)
-    request(
-      "soc/code/#{soc_code}"
+    self.class.get(
+      "/soc/code/#{soc_code}"
     )
-  end
-
-  private
-
-  def request(path)
-    HTTParty.get(BASE_URL + path)
   end
 end
