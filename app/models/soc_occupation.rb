e8118aabfc8455a64c91db441cb1663b89ff5c41
camilleldn <camille@camillebaldock.co.uk>
2016-01-14 11:45:23 +0000

    Refactor with first_or_create

--- app/models/soc_occupation.rb
+++ app/models/soc_occupation.rb
@@ -4,13 +4,10 @@ class SocOccupation < ActiveRecord::Base
   has_many :occupations
 
   def self.find_or_import_from_lmi(soc_code)
-    soc_occupation_lookup = where(soc_code: soc_code)
-    if soc_occupation_lookup.empty?
+    where(soc_code: soc_code).first_or_create do |soc_occupation|
       lmi_client = LmiClient.new
       soc_occupation_lookup = LmiForAll.new(lmi_client).lookup(soc_code)
-      create(soc_occupation_lookup)
-    else
-      soc_occupation_lookup.first
+      soc_occupation.assign_attributes(soc_occupation_lookup)
     end
   end
 end
