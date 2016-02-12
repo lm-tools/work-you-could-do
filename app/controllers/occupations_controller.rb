a598321d6ac36d4084df5986c85f55572c784801
Gareth Visagie <gareth@gjvis.com>
2016-02-12 13:34:19 +0000

    Redirect /occupations to the root path

--- app/controllers/occupations_controller.rb
+++ app/controllers/occupations_controller.rb
@@ -1,4 +1,8 @@
 class OccupationsController < ApplicationController
+  def index
+    redirect_to root_path
+  end
+
   def show
     @occupation = Occupation.find_by!(soc_code: params[:soc_code])
   end
