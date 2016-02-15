4d66b43d66fda61d63ebfc7f82eff1e566141c3b
Gareth Visagie <gareth@gjvis.com>
2016-02-15 12:18:42 +0000

    Everything happens within a "scrapbook"
    A scrapbook is identified by a UUID which is prepended to all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root 'searches#new'
-  resource :search, only: [:show]
-  resources :occupations, only: [:show, :index], param: :soc_code
+
+  root to: 'scrapbooks#create'
+  resources :scrapbooks,
+            path: '/',
+            only: [:show],
+            constraints: { id: Scrapbook::VALID_ID } do
+    resource :search, only: [:new, :show]
+    resources :occupations, only: [:show, :index], param: :soc_code
+    resources :saved_occupations, only: [:create, :destroy], param: :soc_code
+  end
 end
--- spec/controllers/occupations_controller_spec.rb
+++ spec/controllers/occupations_controller_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe OccupationsController do
   describe "#index" do
     it "redirects to the search page" do
-      get :index
+      get :index, scrapbook_id: Scrapbook.new_id
 
       expect(response).to redirect_to(root_path)
     end
