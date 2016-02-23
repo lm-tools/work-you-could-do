606746d45307dc3477cd9754e98651fcc22899b2
Gareth Visagie <gareth@gjvis.com>
2016-02-23 16:45:35 +0000

    Display "occupation saved" confirmation in situ

--- features/save_occupations.feature
+++ features/save_occupations.feature
@@ -8,3 +8,10 @@ Scenario: Save an interesting occupation
   Given I have viewed the details of a specific occupation
   When I save the occupation
   Then it should appear in my list of saved occupations
+
+@javascript
+Scenario: Save an interesting occupation on a javascript-enabled device
+  Given I have viewed the details of a specific occupation
+  When I save the occupation
+  Then I should see an in-situ confirmation that the occupation is saved
+  And it should appear in my list of saved occupations
--- features/step_definitions/save_occupations_steps.rb
+++ features/step_definitions/save_occupations_steps.rb
@@ -10,3 +10,15 @@ end
 Then(/^it should appear in my list of saved occupations$/) do
   click_on("scrapbook")
 end
+
+Then(/^I should see an in\-situ confirmation that the occupation is saved$/) do
+  view_occupation_details_path = \
+    scrapbook_occupation_path(scrapbook_id: scrapbook_id,
+                              soc_code: specific_search_result.fetch(:soc))
+
+  # confirmation displays in-situ
+  expect(current_path).to eq view_occupation_details_path
+
+  confirmation_message = %("#{specific_search_result.fetch(:title)}" saved)
+  expect(page).to have_content(confirmation_message)
+end
