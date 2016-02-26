cf76a33f5055e9578c3b15d2fe5bc5a256fb57bf
Gareth Visagie <gareth@gjvis.com>
2016-02-26 10:40:03 +0000

    Vendorize elements
    This is to allow automatic deployment and review apps on Heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,11 +4,11 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.application.root.join("vendor/assets")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( 
+Rails.application.config.assets.precompile += %w(
 	favicon.ico
 	apple-touch-icon-152x152.png
 	apple-touch-icon-120x120.png
