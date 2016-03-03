1b6ebbef27e027ae43e4c79226d5cb3292dad335
Gareth Visagie <gareth@gjvis.com>
2016-03-03 14:21:41 +0000

    Refactor saved_occupations_controller

--- app/controllers/saved_occupations_controller.rb
+++ app/controllers/saved_occupations_controller.rb
@@ -2,19 +2,22 @@ class SavedOccupationsController < ApplicationController
   before_action :ensure_current_scrapbook
 
   def create
-    # rubocop:disable Lint/HandleExceptions
     @query = params[:query]
     @occupation = Occupation.find_by(soc_code: params[:soc_code])
-
-    begin
-      current_scrapbook.occupations << @occupation
-    rescue ActiveRecord::RecordNotUnique
-      # ignore duplicate entries
-    end
+    ensure_occupation_saved_to_current_scrapbook(@occupation)
   end
 
   def destroy
     @occupation = Occupation.find_by(soc_code: params[:soc_code])
     current_scrapbook.occupations.delete(@occupation)
+  end
+
+  private
+
+  def ensure_occupation_saved_to_current_scrapbook(occupation)
+    # rubocop:disable Lint/HandleExceptions
+    current_scrapbook.occupations << occupation
+  rescue ActiveRecord::RecordNotUnique
+    # ignore duplicate entries
   end
 end
