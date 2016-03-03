143ab76f448fd8a01754cfc44c676107510f3c50
Gareth Visagie <gareth@gjvis.com>
2016-03-03 18:05:14 +0000

    Indent using spaces in config/assets.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,12 +9,12 @@ Rails.application.config.assets.paths << Rails.application.root.join("vendor/ass
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
-	favicon.ico
-	apple-touch-icon-152x152.png
-	apple-touch-icon-120x120.png
-	apple-touch-icon-76x76.png
-	apple-touch-icon-60x60.png
-	opengraph-image.png
-	gov.uk_logotype_crown_invert_trans.png
+  favicon.ico
+  apple-touch-icon-152x152.png
+  apple-touch-icon-120x120.png
+  apple-touch-icon-76x76.png
+  apple-touch-icon-60x60.png
+  opengraph-image.png
+  gov.uk_logotype_crown_invert_trans.png
   gov.uk_logotype_crown.svg
 )
