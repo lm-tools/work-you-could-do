4277805e9368f530766b8d582360402e65cc1a0a
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2017-02-20 14:02:01 +0000

    Add flag to ignore VCR for tests
    - run tests against reale LMI endpoint

--- spec/vcr.rb
+++ spec/vcr.rb
@@ -5,3 +5,8 @@ VCR.configure do |c|
   c.hook_into :webmock
   c.configure_rspec_metadata!
 end
+
+unless ENV["VCR_OFF"].nil?
+  WebMock.allow_net_connect!
+  VCR.turn_off!(ignore_cassettes: true)
+end
