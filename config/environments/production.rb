74751c79d4783ca2ee7fb6578b13c80a875d2c88
Not My Monkeys <notmymonkeys3000@gmail.com>
2017-08-15 11:58:11 +0100

    Json format for logs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,9 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
+  config.lograge.formatter = Lograge::Formatters::Logstash.new
+  config.lograge.enabled = true
+
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
